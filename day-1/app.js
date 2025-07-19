5d5fbf6cfed43085ff6112464cfcd6eb3301ea0d
Ravikant singh <sravikant302@gmail.com>
2025-07-20 00:41:01 +0530

    Add README with Node.js basic guide

---
+++ day-1/app.js
@@ -0,0 +1,13 @@
+// console.log(30+20)
+// console.log("ravikant singh")
+// function fruit(item){
+//     console.log("item name"+item);
+// }
+// fruit('ravikant singh rajput');
+
+
+var fs= require('fs');
+fs.writeFileSync("day-1.txt","my name is ravikant singh")
+
+
+
